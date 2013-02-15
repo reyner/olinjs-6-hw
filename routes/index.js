204a6481d1341d42b25adcd92885c23660095dfc
Reyner Crosby <reyner@gmail.com>
2013-02-15 01:25:06 -0500

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,5 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
   req.facebook.api('/2014182723480/photos', function(err, data){
-    // res.send(data);
     res.render('index', { title: 'Cover Photos', data: data.data });
   });
 };
--- routes/intermediate.js
+++ routes/intermediate.js
@@ -1,8 +1,3 @@
-
-/*
- * GET users listing.
- */
-
 exports.comment = function(req, res){
   console.log("Trying to post to "+req.body.fbid)
   req.facebook.api('/'+req.body.fbid+'/comments', 'POST', {'message': req.body.comment}, function(err, stuff) {
